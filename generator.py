f2527accce072a9548713f5496a0e9c1c288d40f
lzl32947 <lzl32947@outlook.com>
2021-11-30 20:23:22 +0800

    feat(Data_generator): Update a basic data generator for mixup
    Update a basic data generator for mixup

--- generator.py
+++ generator.py
@@ -1,2 +1,34 @@
-def mix_up():
-    pass
+import numpy as np
+
+from functional.datasets.mixup_dataset import MixupDataset
+import torchvision.transforms as transforms
+import torch.utils.data as data
+
+
+def mix_up_random(dataset_path):
+    trainset = MixupDataset(transform=None, path=dataset_path)
+    trainloader = data.DataLoader(trainset, batch_size=1, shuffle=True, num_workers=0)
+    data_list = []
+    label_list = []
+    count = 1
+    for d, l in trainloader:
+        if count > 50000:
+            break
+        d = d.numpy().astype(np.uint8)
+        l = l.numpy()
+
+        data_list.append(d)
+        label_list.append(l)
+        count += 1
+        if count % 100 == 0:
+            print(count)
+    data_list = np.array(data_list)
+    data_list = np.squeeze(data_list)
+    label_list = np.array(label_list)
+    label_list = np.squeeze(label_list)
+    np.save("output/data.npy", data_list)
+    np.save("output/label.npy", label_list)
+
+
+if __name__ == '__main__':
+    mix_up_random("dataset/cifar_10_standard_train")
