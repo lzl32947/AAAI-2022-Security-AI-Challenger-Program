cb329c985b2edc65792fe6dac354cd14e610d4cb
lzl32947 <lzl32947@outlook.com>
2021-11-30 20:29:11 +0800

    docs(Log): Update logs
    1. Adding all logs into repo

---
+++ log/cifar10/20211129_165807/train_config.py
@@ -0,0 +1,28 @@
+# This is the original config files from https://github.com/vtddggg/training_template_for_AI_challenger_sea8/blob/main/config.py
+# Modifies should be commented
+
+args_resnet = {
+    'epochs': 200,  # 200 for original
+    'optimizer_name': 'SGD',
+    'optimizer_hyperparameters': {
+        'lr': 0.1,
+        'momentum': 0.9,
+        'weight_decay': 1e-4
+    },
+    'scheduler_name': 'CosineAnnealingLR',
+    'scheduler_hyperparameters': {
+        'T_max': 200
+    },
+    'batch_size': 128,  # 256 for original
+}
+args_densenet = {
+    'epochs': 200,
+    'optimizer_name': 'SGD',
+    'optimizer_hyperparameters': {
+        'lr': 0.1,
+        'momentum': 0.9,
+        'weight_decay': 1e-4
+    },
+    'scheduler_name': None,
+    'batch_size': 128,  # 256 for original
+}
